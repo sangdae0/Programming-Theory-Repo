f8ac1d9e5238fd3405f67ed20504d8e5ab4263b4
Sangdae Kim <6344700+sangdae0@users.noreply.github.com>
2024-05-25 16:39:21 +0900

    title screen
    - added title screen scene

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+	public void MoveToTestScene()
+	{
+		SceneManager.LoadScene("TestScene");
+	}
+}
